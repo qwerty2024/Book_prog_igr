a9edc79b92b59a30f45b8cdda9dbe579b1b8c98f
qwerty2024 <multanmgu@gmail.com>
2024-09-17 12:12:03 +0300

    Создал и написал Game stats2.

---
+++ Game_stats2/Game_stats2/Game_stats2.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned int score = 5000;
+    score = score + 1000;
+    cout << "score: " << score << endl;
+
+    score += 100;
+    cout << "score: " << score << endl;
+
+    int lives = 3;
+    ++lives;
+    cout << "lives: " << lives << endl;
+    lives = 3;
+    lives++;
+    cout << "lives: " << lives << endl;
+    lives = 3;
+    int bonus = ++lives * 10;
+    cout << "lives.bonus = " << lives << "." << bonus << endl;
+    lives = 3;
+    bonus = lives++ * 10;
+    cout << "lives.bonus = " << lives << "." << bonus << endl;
+
+    score = 4294967295;
+    cout << "\nscore: " << score << endl;
+    ++score;
+    cout << "\nscore: " << score << endl;
+
+    return 0;
+}
