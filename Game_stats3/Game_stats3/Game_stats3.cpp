bd8af3a65d7caeba69a806da472a02e8a3ed8723
qwerty2024 <multanmgu@gmail.com>
2024-09-17 12:23:42 +0300

    Создал и написал Game stats3.

---
+++ Game_stats3/Game_stats3/Game_stats3.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int ALIEN_POINTS = 150;
+    int alienKilled = 10;
+    int score = alienKilled * ALIEN_POINTS;
+    cout << "score: " << score << endl;
+
+    enum difficulty {NOVICE, EASY, NORMAL, HARD, UNBEATABLE};
+
+    difficulty myDifficalty = EASY;
+
+    enum shipCost {FIGHTER_COST = 25, BOMBER_COST, CRUISER_COST = 50};
+
+    shipCost myShipCost = BOMBER_COST;
+
+    cout << "\nTo upgrade my ship to a Cruiser will cost " <<
+        (CRUISER_COST - myShipCost) << " Resource Pooints.\n";
+
+    return 0;
+}
